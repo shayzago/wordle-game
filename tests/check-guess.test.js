ff240ed1925da57232bbe66177dbd687cde3e157
shayzago <zagoshayenne@gmail.com>
2024-09-20 11:44:02 -0300

    :white_check_mark: test: adicionando as notificações

--- tests/check-guess.test.js
+++ tests/check-guess.test.js
@@ -2,18 +2,25 @@ const { JSDOM } = require("jsdom");
 const path = require("path");
 const app = require("../resources/scripts/app");
 
-const NOTIFICATION_EMPTY_GUESS = "Empty guess";
-const NOTIFICATION_ENTER_KEY_PRESSED = "Enter key pressed";
-const NOTIFICATION_INCOMPLETE_GUESS = "Incomplete guess";
-const NOTIFICATION_WORD_NOT_IN_DATABASE = "Word not in database";
-const NOTIFICATION_GAME_OVER_GUESS_RIGHT = "You guessed right! Game over!";
+const {
+  NOTIFICATION_EMPTY_GUESS,
+  NOTIFICATION_ENTER_KEY_PRESSED,
+  NOTIFICATION_GAME_OVER_GUESS_RIGHT,
+  NOTIFICATION_INCOMPLETE_GUESS,
+  NOTIFICATION_WORD_NOT_IN_DATABASE,
+  GREEN_COLOR_RGB,
+} = require("./auxi/consts");
 
-const GREEN_COLOR_RGB = "rgb(83, 141, 78)";
+const { getGameBoardLetter } = require("./auxi/helpers");
 
-const getGameBoardLetter = (index) => {
-  return global.document.querySelector(`.letter-${index}`).style
-    .backgroundColor;
-};
+const {
+  mockToastify,
+  unMockToastify,
+  toastifyDefaultConfig,
+  TOASTIFY_SUCCESS_COLOR,
+  TOASTIFY_ERROR_COLOR,
+  TOASTIFY_WARNING_COLOR,
+} = require("./mocks/toastify");
 
 describe("Checking guess", () => {
   const database = [
@@ -32,13 +39,15 @@ describe("Checking guess", () => {
     );
     global.document = dom.window.document;
     global.window = dom.window;
+    global.Toastify = mockToastify();
   });
 
   afterEach(() => {
     jest.restoreAllMocks();
+    unMockToastify();
   });
 
-  test('should return a "Empty guess" message because currentGuess attribute is empty', () => {
+  test('should show a notification with "Empty guess" message because currentGuess attribute is empty', () => {
     const game = {
       database,
       currentRow: 1,
@@ -47,10 +56,16 @@ describe("Checking guess", () => {
       rightGuess: "lunch",
     };
 
-    expect(app.checkGuess(game)).toBe(NOTIFICATION_EMPTY_GUESS);
+    app.checkGuess(game);
+    expect(global.Toastify).toHaveBeenCalled();
+    expect(global.Toastify).toHaveBeenCalledWith({
+      ...toastifyDefaultConfig,
+      text: NOTIFICATION_EMPTY_GUESS,
+      backgroundColor: TOASTIFY_ERROR_COLOR,
+    });
   });
 
-  test('should return a "Incomplete guess" message because currentGuess attribute size is less than 5 letter', () => {
+  test('should show notification with a "Incomplete guess" message because currentGuess attribute size is less than 5 letter', () => {
     const game = {
       database,
       currentRow: 1,
@@ -59,7 +74,13 @@ describe("Checking guess", () => {
       rightGuess: "sorry",
     };
 
-    expect(app.checkGuess(game)).toBe(NOTIFICATION_INCOMPLETE_GUESS);
+    app.checkGuess(game);
+    expect(global.Toastify).toHaveBeenCalled();
+    expect(global.Toastify).toHaveBeenCalledWith({
+      ...toastifyDefaultConfig,
+      text: NOTIFICATION_INCOMPLETE_GUESS,
+      backgroundColor: TOASTIFY_WARNING_COLOR,
+    });
   });
 
   test('should return a "Word not in database" message because currentGuess is not in database list', () => {
@@ -71,7 +92,13 @@ describe("Checking guess", () => {
       rightGuess: "sorry",
     };
 
-    expect(app.checkGuess(game)).toBe(NOTIFICATION_WORD_NOT_IN_DATABASE);
+    app.checkGuess(game);
+    expect(global.Toastify).toHaveBeenCalled();
+    expect(global.Toastify).toHaveBeenCalledWith({
+      ...toastifyDefaultConfig,
+      text: NOTIFICATION_WORD_NOT_IN_DATABASE,
+      backgroundColor: TOASTIFY_WARNING_COLOR,
+    });
   });
 
   test('should return a "Enter key pressed" when current guess is not correct', () => {
@@ -90,8 +117,6 @@ describe("Checking guess", () => {
   });
 
   test("when the guess match with right word", () => {
-    global.alert = jest.fn(() => NOTIFICATION_GAME_OVER_GUESS_RIGHT);
-
     const game = {
       database,
       currentRow: 1,
@@ -100,7 +125,7 @@ describe("Checking guess", () => {
       rightGuess: "sorry",
     };
 
-    expect(app.checkGuess(game)).toBe(NOTIFICATION_GAME_OVER_GUESS_RIGHT);
+    app.checkGuess(game);
 
     expect(game.currentGuess).toBe("sorry");
     expect(game.currentLetterPosition).toBe(6);
@@ -111,5 +136,12 @@ describe("Checking guess", () => {
     expect(getGameBoardLetter(3)).toBe(GREEN_COLOR_RGB);
     expect(getGameBoardLetter(4)).toBe(GREEN_COLOR_RGB);
     expect(getGameBoardLetter(5)).toBe(GREEN_COLOR_RGB);
+
+    expect(global.Toastify).toHaveBeenCalled();
+    expect(global.Toastify).toHaveBeenCalledWith({
+      ...toastifyDefaultConfig,
+      text: NOTIFICATION_GAME_OVER_GUESS_RIGHT,
+      backgroundColor: TOASTIFY_SUCCESS_COLOR,
+    });
   });
 });
