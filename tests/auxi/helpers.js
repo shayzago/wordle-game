85658595f5083610194ae62dca61e18464bf9cd8
shayzago <zagoshayenne@gmail.com>
2024-09-22 10:37:00 -0300

    :sparkles: feat: adicionando botão de jogar novamente

--- tests/auxi/helpers.js
+++ tests/auxi/helpers.js
@@ -1,8 +1,20 @@
-const getGameBoardLetter = (index) => {
+const getGameBoardLetterBackgroundColorByRow = (index, row = 1) => {
+  return global.document.querySelector(`.row-${row} .letter-${index}`).style
+    .backgroundColor;
+};
+
+const getGameBoardLetterBackgroundColor = (index) => {
   return global.document.querySelector(`.letter-${index}`).style
     .backgroundColor;
 };
 
+const getPlayAgainButtonVisibility = () => {
+  return global.document.querySelector(`.play-again .btn-play-again`).style
+    .display;
+};
+
 module.exports = {
-  getGameBoardLetter,
+  getGameBoardLetterBackgroundColor,
+  getGameBoardLetterBackgroundColorByRow,
+  getPlayAgainButtonVisibility,
 };
